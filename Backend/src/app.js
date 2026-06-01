4b1eb44aef52b16b4195da10971ed4c32884fadd
Sana Matusala <matusalasana@gmail.com>
2026-06-01 11:58:10 +0300

    feat: create logger middleware to color the endpoints, create health route for test, test logger middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,3 +1,9 @@
 import express from "express";
-
+import { loggerMiddleware } from "./middlewares/logger.middleware.js"
 export const app = express();
+
+
+app.use(loggerMiddleware);
+app.get('/api/v1/health', (req, res) => {
+  res.json({ status: 'ok', timestamp: new Date().toISOString() });
+});
