1ffe27cb3273642857af8cc3aab365060afba685
Sana Matusala <matusalasana@gmail.com>
2026-06-01 15:04:43 +0300

    feat: create hash utils for password and token hashing

---
+++ Backend/src/utils/hash.js
@@ -0,0 +1,40 @@
+import bcrypt from "bcryptjs";
+import { Env } from "../configs/env.js"
+
+if(!Env.SALT_ROUNDS){
+  throw new Error("Hash Error: missing salt from .env")
+};
+
+// PASSWORD HASH AND VERIFICATION 
+const hashPassword = async (password) => {
+  const hashedPassword = await bcrypt.hash(password, Env.SALT_ROUNDS);
+  
+  return hashedPassword;
+}
+
+const comparePassword = async (password, hashedPassword) => {
+  const isMatch = await bcrypt.compare(password, hashedPassword)
+  
+  return isMatch;
+}
+
+// REFRESH TOKEN  AND VERIFICATION
+const hashToken = async (token) => {
+  const hashedToken = await bcrypt.hash(token, Env.SALT_ROUNDS);
+  
+  return hashedToken;
+}
+
+const compareToken = async (token, hashedToken) => {
+  const isMatch = await bcrypt.compare(token, hashedToken)
+  
+  return isMatch;
+}
+
+
+export const HashUtils = {
+  hashPassword,
+  hashToken,
+  comparePassword,
+  compareToken
+}
