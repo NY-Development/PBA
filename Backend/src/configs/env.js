d69c0b10398b53c720d54c0cbe86f27ca4636873
Sana Matusala <matusalasana@gmail.com>
2026-06-01 11:48:35 +0300

    feat: create logger util to save, color, filter, and send logs to cloud logging services

--- Backend/src/configs/env.js
+++ Backend/src/configs/env.js
@@ -4,8 +4,10 @@ dotenv.config();
 
 const DATABASE_URL = process.env.DATABASE_URL;
 const PORT = process.env.PORT;
+const NODE_ENV = process.env.NODE_ENV;
 
 export const Env = {
   DATABASE_URL,
-  PORT
+  PORT,
+  NODE_ENV
 }
