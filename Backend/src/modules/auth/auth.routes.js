09faed760911f319f25d7ebd105fafe7ad1f742b
Sana Matusala <matusalasana@gmail.com>
2026-06-05 01:25:38 +0300

    feat: add zod validation for auth module

--- Backend/src/modules/auth/auth.routes.js
+++ Backend/src/modules/auth/auth.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
 
-import { protect } from "../../middlewares/auth.middleware.js"
+import { protect } from "../../middlewares/auth.middleware.js";
+import { validate } from "../../middlewares/validation.middleware.js";
 import { AuthController } from "./auth.controller.js"
+import { registerSchema, loginSchema } from "./auth.validation.js"
+
+
 const router = Router();
 
-router.post("/register", AuthController.register);
-router.post("/login", AuthController.login);
+router.post("/register", validate(registerSchema), AuthController.register);
+router.post("/login", validate(loginSchema), AuthController.login);
 router.post("/logout", protect, AuthController.logout);
 router.patch("/update", protect, AuthController.update);
 router.post("/refresh", protect, AuthController.refresh);
---
+++ Backend/src/modules/auth/auth.validation.js
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const registerSchema = z.object({
+  first_name: z.string().min(3),
+  last_name: z.string().min(3),
+  email: z.string().email().trim(),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+});
+
+export const loginSchema = z.object({
+  email: z.string().email().trim(),
+  password: z.string().min(8, "Password must be al least 8 characters"),
+});
